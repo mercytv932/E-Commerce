01c1a4add49a650251f1fdb39e9ce16f24f51927
Yosef <mercytv932@gmail.com>
2026-08-14 15:31:11 -0400

    Product methods

--- src/models/product.ts
+++ src/models/product.ts
@@ -18,4 +18,13 @@ export class Product {
     this.price = price;
     this.discountPercentage = discountPercentage;
   }
+
+  displayDetails() {
+    return `This is ${this.title} ${this.category} for ${this.price} ${this.discountPercentage}`;
+  }
+
+  getPriceWithDiscount() {
+    const discountAmount = this.price * (this.discountPercentage / 100); //20/100 = 0.2/20% * 200
+    return Number((this.price - discountAmount).toFixed(2)); // this.price/original/180 - discount/40 = 120
+  }
 }
