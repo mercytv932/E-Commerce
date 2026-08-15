89f7dbb356ea04c16d7a73a22e4ff3fdc744fa55
Yosef <mercytv932@gmail.com>
2026-08-15 19:44:44 -0400

    tax calculation

---
+++ src/utils/taxCalculator.ts
@@ -0,0 +1,10 @@
+export function taxCalculator(price: number, category: string) {
+  let taxPercent = 0.0475;
+
+  if (category === "groceries") {
+    taxPercent = 0.03;
+  }
+
+  const tax = price * taxPercent;
+  return ` $${tax}`;
+}
